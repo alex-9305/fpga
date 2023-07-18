d3c5b84c856dc0a5181dd09b627447c5cd7c5f93
alex <alexander.letzel@yahoo.de>
2023-07-18 13:22:54 +0200

    upload first example and try testbench simulation

---
+++ example_and/example_and.v
@@ -0,0 +1,14 @@
+module example_and_gate
+    (
+        input input_1,
+        input input_2,
+        output and_result
+    );
+
+    wire   and_temp;
+
+    assign and_temp = input_1 & input_2;
+
+    assign and_result = and_temp;
+
+endmodule // example_and_gate
---
+++ example_and/example_and_tb.v
@@ -0,0 +1,37 @@
+`include "example_and.v"
+`timescale 1us/1ns
+
+module example_and_tb;
+    reg input_1    = 1'b0;
+    reg input_2   = 1'b0;
+    wire and_result;
+
+    example_and dut
+        (
+            .input_1(input_1),
+            .input_2(input_2),
+            .and_result(and_result)
+        );
+
+    initial begin
+        $dumpfile("example_and_test.vcd");
+        $dumpvars(0, example_and_tb);
+    end
+
+    initial begin
+        #10
+        input_1 <= 1'b1;
+        #10
+        input_2 <= 1'b1;
+        #10
+        input_1 <= 1'b0;
+        #10
+        input_2 <= 1'b0;
+        #20
+        input_1 <= 1'b1;
+        input_2 <= 1'b1;
+        #10
+        $display("Test Complete");
+    end
+endmodule
+
