14fa6c0a98d280a039bf777677424152c13ba72a
alex <alexander.letzel@yahoo.de>
2023-07-28 11:38:53 +0200

    Fixes window_fsm

--- windows_fsm/window_fsm.v
+++ windows_fsm/window_fsm.v
@@ -1,22 +1,63 @@
 module window_fsm
     (
         input   wire button_press, n_reset, clk,
-        output  wire open_cw, close_ccw
+        output  reg open_cw, close_ccw
     );
 
     localparam  STATE_CLOSED = 1'b0,
                 STATE_OPENED = 1'b1;
 
-    wire next_state = STATE_CLOSED;
+    reg next_state = STATE_CLOSED;
     reg current_state = STATE_CLOSED;
 
     //next state logic
-    assign next_state = ((current_state == STATE_CLOSED) && (button_press == 1'b0)) ? STATE_CLOSED:
-                        ((current_state == STATE_CLOSED) && (button_press == 1'b1)) ? STATE_OPENED:
-                        ((current_state == STATE_OPENED) && (button_press == 1'b0)) ? STATE_OPENED:
-                        ((current_state == STATE_OPENED) && (button_press == 1'b1)) ? STATE_CLOSED:
-                        (n_reset == 1'b0) ? STATE_CLOSED:
-                        1'bX;
+    always @(current_state, button_press, n_reset) begin
+        if (n_reset == 0) begin
+            next_state <= STATE_CLOSED;
+        end else begin
+            case(current_state)
+                STATE_CLOSED:
+                    if(button_press == 1'b0) begin
+                        next_state <= STATE_CLOSED;
+                    end else begin
+                        next_state <= STATE_OPENED;
+                    end
+                STATE_OPENED:
+                    if(button_press == 1'b0) begin
+                        next_state <= STATE_OPENED;
+                    end else begin
+                        next_state <= STATE_CLOSED;
+                    end
+            endcase
+        end
+    end
+
+    //output logic
+    always @(current_state, button_press, n_reset) begin
+        if (n_reset == 0) begin
+            open_cw <= 1'b0;
+            close_ccw <= 1'b0;
+        end else begin
+            case(current_state)
+                STATE_CLOSED:
+                    if(button_press == 1'b0) begin
+                        close_ccw <= 1'b0;
+                        open_cw <= 1'b0;
+                    end else begin
+                        close_ccw <= 1'b0;
+                        open_cw <= 1'b1;
+                    end
+                STATE_OPENED:
+                    if(button_press == 1'b0) begin
+                        open_cw <= 1'b0;
+                        close_ccw <= 1'b0;
+                    end else begin
+                        open_cw <= 1'b0;
+                        close_ccw <= 1'b1;
+                    end
+            endcase
+        end
+    end
 
     //current state logic
     always @(posedge clk) begin
@@ -26,18 +67,5 @@ module window_fsm
             current_state <= next_state;
         end
     end
-
-    //output logic
-    assign open_cw =    ((current_state == STATE_CLOSED) && (button_press == 1'b0)) ? 1'b0:
-                        ((current_state == STATE_CLOSED) && (button_press == 1'b1)) ? 1'b1:
-                        (current_state == STATE_OPENED) ? 1'b0:
-                        (n_reset == 1'b0) ? 1'b0:
-                        1'bX;
-
-    assign close_ccw =  ((current_state == STATE_OPENED) && (button_press == 1'b0)) ? 1'b0:
-                        ((current_state == STATE_OPENED) && (button_press == 1'b1)) ? 1'b1:
-                        (current_state == STATE_CLOSED) ? 1'b0:
-                        (n_reset == 1'b0) ? 1'b0:
-                        1'bX;
 
 endmodule
